7cffa66dce2ffe7f85be011d12d3a9891544d0d5
gokaySendil <76568340+gokaySendil@users.noreply.github.com>
2021-09-14 17:24:34 +0300

    Add files via upload

---
+++ CurrencyConverter_without_GUI/src/gokaySendil/Main.java
@@ -0,0 +1,11 @@
+package gokaySendil;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        CurrencyConverter c = new CurrencyConverter();
+        c.showMenu();
+    }
+}
